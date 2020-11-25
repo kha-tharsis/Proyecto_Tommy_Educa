fe62eefd24ffd8792665426b245bcace1355e4ad
ThePhoenixxD <dieguithox2000@gmail.com>
2020-11-25 14:58:10 -0300

    Creacion_de_Modelo_y_BD_Colegio

--- app/src/main/java/com/example/proyectotommyeduca/data/BD.java
+++ app/src/main/java/com/example/proyectotommyeduca/data/BD.java
@@ -1,4 +1,37 @@
 package com.example.proyectotommyeduca.data;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 
-public class BD {
+import androidx.annotation.Nullable;
+
+public class BD extends SQLiteOpenHelper{
+    private static String NAME = "bd_Videos";
+    private static int VERSION = 1;
+    private final String tabla_colegio = "create table colegio(colegio_id integer primary key autoincrement, " +
+                                         "colegio_nombre text, latitud float, longitud float)";
+    /*      Colegio
+     *private int colegio_id;
+     *private  String colegio_nombre;
+     *private float latitud, longitud;
+     */
+
+    public BD(Context context) {
+        super(context, NAME, null, VERSION);
+
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(tabla_colegio);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
+
+
 }
